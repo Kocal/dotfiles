a2cb4d4595217b27492d3b802241a2ae9e5bcadc
Hugo Alliaume <hugo@alliau.me>
2026-08-10 22:06:19 +0200

    update vscode deps

--- nix/home/vscode.nix
+++ nix/home/vscode.nix
@@ -19,20 +19,32 @@
         # Current set
         l13rary.l13-diff
         ms-azuretools.vscode-containers
+        ms-azuretools.vscode-docker
         jnoortheen.nix-ide
         bmewburn.vscode-intelephense-client
         waderyan.gitblame
         mblode.twig-language-2
         ms-vscode-remote.remote-ssh
+        ms-vscode-remote.remote-ssh-edit # hard dependency of remote-ssh
+        ms-vscode.remote-explorer
         matthewpi.caddyfile-support
 
         # Vue + JS/TS
         vue.volar
+        bradlc.vscode-tailwindcss
+        unifiedjs.vscode-mdx
 
         # Symfony/PHP dev extras
         editorconfig.editorconfig
         redhat.vscode-yaml
         neilbrayfield.php-docblocker
+        devsense.composer-php-vscode
+
+        # UX / theming / locale
+        usernamehw.errorlens
+        github.github-vscode-theme
+        k--kato.intellij-idea-keybindings
+        ms-ceintl.vscode-language-pack-fr
       ];
     };
   };
